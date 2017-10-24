a885fa3c3a58cfde13942da2eb4e355a1afc7fac
Gonzalo Aguayo <gonzalo.aguayo@jalasoft.com>
2017-10-24 00:06:53 -0400

    Deleting unnecesary public in client folder

--- client/gulp/build.js
+++ client/gulp/build.js
@@ -96,7 +96,8 @@ gulp.task('clean', function () {
   return $.del([path.join(conf.paths.dist, '/'), 
     path.join('!' + conf.paths.dist, '/index.php'),
     path.join('!' + conf.paths.dist, '/.htaccess'),
-    path.join(conf.paths.tmp, '/')]);
+    path.join(conf.paths.tmp, '/')], { force: true }
+  );
 });
 
 gulp.task('build', ['html', 'fonts', 'other']);
